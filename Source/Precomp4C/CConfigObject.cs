ff1c8cc889e644b3744222b9435c5cc259b36786
Ratin <ratin@knsoft.org>
2022-06-04 22:43:52 +0800

    Initial commit

---
+++ Source/Precomp4C/CConfigObject.cs
@@ -0,0 +1,28 @@
+﻿namespace Precomp4C {
+    public class CO_I18N {
+        public string Pattern { get; set; }
+        public string Default { get; set; }
+    }
+    public class CO_SymbolExtract_Symbol {
+        public string Name { get; set; }
+        public string Export { get; set; }
+    }
+    public class CO_SymbolExtract {
+        public string File { get; set; }
+        public CO_SymbolExtract_Symbol[] Symbols { get; set; }
+    }
+    public class CO_Binary {
+        public string File { get; set; }
+        public string Export { get; set; }
+    }
+    public class CO_Loaders {
+        public CO_I18N I18N { get; set; }
+        public CO_SymbolExtract[] SymExtract { get; set; }
+        public CO_Binary[] Binary { get; set; }
+    }
+    class CConfigObject {
+        public string NTAssassinIntegrate { get; set; }
+        public CO_Loaders Loaders { get; set; }
+        public string Output { get; set; }
+    }
+}
---
+++ Source/Precomp4C/Loaders/Binary.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+namespace Precomp4C.Loaders {
+    static class Binary {
+        public static DateTime GetLastModifyTime(string ProjectDir, CO_Binary[] ConfigObject) {
+            DateTime LastModifyTime = DateTime.MinValue;
+            foreach (var BinaryItem in ConfigObject) {
+                string BinFilePath = Path.GetFullPath(ProjectDir + BinaryItem.File);
+                if (!File.Exists(BinFilePath)) {
+                    RTL.RaiseError(200, "Binary file (" + BinFilePath + ") not exists");
+                }
+                DateTime SourceModifyTime = File.GetLastWriteTime(BinFilePath);
+                if (SourceModifyTime > LastModifyTime) {
+                    LastModifyTime = SourceModifyTime;
+                }
+            }
+            return LastModifyTime;
+        }
+        public static bool Process(string ProjectDir, FileStream fsHeader, FileStream fsSource, CO_Binary[] ConfigObject) {
+            Stopwatch Elapsed = new();
+            Elapsed.Start();
+            Console.WriteLine("Binary Loader starts...");
+
+            foreach (var BinaryItem in ConfigObject) {
+                string BinFilePath = Path.GetFullPath(ProjectDir + BinaryItem.File);
+                Console.WriteLine("Binary Loader - Compiling: " + BinFilePath);
+                FileStream fsBinFile = null;
+                FileInfo BinFile = null;
+                try {
+                    BinFile = new FileInfo(BinFilePath);
+                    fsBinFile = BinFile.OpenRead();
+                } catch {
+                    RTL.RaiseError(301, "Open binary file (" + BinFilePath + ") failed");
+                }
+                if (BinFile.Length <= 0) {
+                    RTL.RaiseError(302, "Binary file (" + BinFilePath + ") is empty!");
+                }
+                var Data = new byte[BinFile.Length];
+                RTL.ReadStream(fsBinFile, Data, 0);
+                var SymbolDef = string.Format("unsigned char {0}[{1:D}]", "BIN_" + BinaryItem.Export, BinFile.Length);
+                int RemainSize = Convert.ToInt32(BinFile.Length);
+                uint BytesPerLine = 25;
+                RTL.WriteOutput(fsHeader, "extern " + SymbolDef + ";");
+                RTL.WriteOutput(fsSource, SymbolDef + " = {");
+                while (RemainSize > 0) {
+                    var LineSize = RemainSize > BytesPerLine ? BytesPerLine : Convert.ToUInt32(RemainSize);
+                    var Bytes = new string[LineSize];
+                    for (var j = 0; j < LineSize; j++)
+                        Bytes[j] = string.Format("0x{0:X2}", Data[BinFile.Length - RemainSize--]);
+                    RTL.WriteOutput(fsSource, "\t" + string.Join(", ", Bytes) + (RemainSize == 0 ? null : ","));
+                }
+                RTL.WriteOutput(fsSource, "};\r\n");
+            }
+
+            Elapsed.Stop();
+            Console.WriteLine("Binary Loader finished, elapsed {0:G}ms", Elapsed.Elapsed.TotalMilliseconds);
+            return true;
+        }
+    }
+}
